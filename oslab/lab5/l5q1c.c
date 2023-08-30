119247e6ca34af7dae5665a63f5945eee3e09154
viswatara <99180943+viswatara@users.noreply.github.com>
2023-08-30 10:59:41 +0530

    Update l5q1c.c

--- oslab/lab5/l5q1c.c
+++ oslab/lab5/l5q1c.c
@@ -7,23 +7,18 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #define FIFO_NAME "/tmp/my_fifo"
-#define BUFFER_SIZE 7
 int main() {
 	int pipe_fd;
 	int res;
 	int open_mode = O_RDONLY;
-	char buffer[BUFFER_SIZE];
-	int bytes_read = 0;
+	char buffer[4];
 	memset(buffer, '\0', sizeof(buffer));
 	printf("Process %d opening FIFO O_RDONLY\n", getpid());
 	pipe_fd = open(FIFO_NAME, open_mode);
 	printf("Process %d result %d\n", getpid(), pipe_fd);
 	if (pipe_fd != -1) {
-			
-			res = read(pipe_fd, buffer,4);
-			printf("%s ",buffer);
-
-		
+		res = read(pipe_fd, buffer,4);
+		printf("%s ",buffer);
 		(void)close(pipe_fd);
 	}
 	else {
